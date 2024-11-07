acc720c05718e4a898b9a0311bb3b5f9bc97d12e
Vitor Santos <the_vitor@hotmail.com>
2024-11-06 23:31:38 -0300

    nova versao

--- public/scripts/cadastro.js
+++ public/scripts/cadastro.js
@@ -21,6 +21,19 @@ document.getElementById("presente-form").addEventListener("submit", async (e) =>
   }
 });
 
+document.getElementById("gerar20Itens").addEventListener("click", async function() {
+  for(let i=1; i <= 20; i++){
+    let num = Math.random();
+    let descricao = `item ${num}`;
+    await fetch("/add-item", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ descricao }),
+    });
+  }
+  fetchItems();
+});
+
 // Função para buscar os itens e atualizar a lista
 async function fetchItems() {
   const response = await fetch("/get-items");
@@ -35,4 +48,41 @@ async function fetchItems() {
     listItem.textContent = `Presente: ${descricao} | Presentiador: ${nome_pessoa}`;
     listContainer.appendChild(listItem);
   });
+}
+
+function importarArquivo() {
+  const inputArquivo = document.getElementById("arquivoItens");
+  if (inputArquivo.files.length === 0) {
+      alert("Selecione um arquivo para importação");
+      return;
+  }
+
+  const arquivo = inputArquivo.files[0];
+  const leitor = new FileReader();
+
+  leitor.onload = function(evento) {
+      const conteudo = evento.target.result;
+      processarConteudo(conteudo);
+  };
+  leitor.onerror = function() {
+      console.error("Erro ao ler o arquivo.");
+  };
+  leitor.readAsText(arquivo);
+  document.getElementById("arquivoItens").value = "";
+  alert("Lista importada com sucesso.");
+}
+
+function processarConteudo(conteudo) {
+  const linhas = conteudo.split("\n");
+  linhas.forEach(linha => {
+      const [descricao, pessoa] = linha.split(",");
+      if (descricao) {
+        fetch("/add-item", {
+          method: "POST",
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify({ descricao, pessoa }),
+        });
+      }
+  });
+  fetchItems();
 }
