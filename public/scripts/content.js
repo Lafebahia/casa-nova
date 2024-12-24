c90ad0efa2bfe9bccd4aa9bdce4f39deb989125d
Vitor Santos <the_vitor@hotmail.com>
2024-12-24 20:01:42 -0300

    ajustes e finalizando sobre o evento

--- public/scripts/content.js
+++ public/scripts/content.js
@@ -5,3 +5,11 @@ fetch('/templates/navbar.html').then(response => response.text()).then(data => {
 fetch('/templates/footer.html').then(response => response.text()).then(data => {
     document.getElementById('footer-page').innerHTML = data;
 });
+
+function solicitarUber() {
+    const latitude = -12.9053012;
+    const longitude = -38.4883505;
+    const nomeDoDestino = "São João do Cabrito";
+    const url = `https://m.uber.com/ul/?action=setPickup&pickup=my_location&dropoff[latitude]=${latitude}&dropoff[longitude]=${longitude}&dropoff[nickname]=${encodeURIComponent(nomeDoDestino)}`;
+    window.open(url, '_blank');
+}
