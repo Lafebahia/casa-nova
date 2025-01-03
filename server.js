d8b210da199b6e20c956de6ae4e64fa95960a9a8
Vitor Santos <the_vitor@hotmail.com>
2025-01-03 15:19:47 -0300

    chave de acesso cadastro

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const sqlite3 = require("sqlite3").verbose();
 const https = require("https");
 const fs = require("fs");
 const path = require("path");
+const crypto = require('crypto');
 const app = express();
 const db = new sqlite3.Database("./database.db");
 require('dotenv').config();
@@ -13,6 +14,25 @@ const options = {
   cert: fs.readFileSync(path.join(__dirname, '../certificado/certificado.crt')),
   ca: fs.readFileSync(path.join(__dirname, '../certificado/CA.crt'))
 };
+
+function generateAppKey() {
+  return crypto.randomBytes(16).toString('hex');
+}
+
+if (!process.env.APP_KEY || process.env.APP_KEY.trim() === '') {
+  const appKey = generateAppKey();
+  const envFilePath = '.env';
+  let envFileContent = fs.readFileSync(envFilePath, 'utf-8');
+  if (envFileContent.includes('APP_KEY=')) {
+      envFileContent = envFileContent.replace(/APP_KEY=\s*(.*)/, `APP_KEY=${appKey}`);
+  } else {
+      envFileContent += `APP_KEY=${appKey}\n`;
+  }
+  fs.writeFileSync(envFilePath, envFileContent, 'utf-8');
+  console.log('chave de acesso gerada com sucesso');
+}
+
+const validAppKey = process.env.APP_KEY;
 
 app.use(bodyParser.json());
 app.use(express.static("public"));
@@ -25,6 +45,10 @@ app.get("/", (req, res) => {
 });
 
 app.get("/cadastro", (req, res) => {
+    const chave = req.query.chave;
+    if (!chave || chave !== validAppKey) {
+        return res.status(403).send('Acesso não permitido: chave inválida ou ausente');
+    }
     res.sendFile(__dirname + "/public/cadastro.html");
 });
 
@@ -78,6 +102,11 @@ app.delete("/delete-item/:id", (req, res) => {
 });
   
 const PORT = process.env.SERVER_PORT || 3000;
+// server
 https.createServer(options, app).listen(PORT, () => {
   console.log(`Servidor HTTPS rodando na porta ${PORT}`);
 });
+// localhost
+// app.listen(PORT, () => {
+//   console.log(`Servidor rodando na porta ${PORT}`);
+// });
